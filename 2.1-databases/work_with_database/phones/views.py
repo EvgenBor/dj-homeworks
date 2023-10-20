6fb7380d8d4da11d6c9cbebc4c4cc2844b945e00
Evgeny Bor <evbor@mail.ru>
2023-10-20 22:54:33 +0500

    work with orm

--- 2.1-databases/work_with_database/phones/views.py
+++ 2.1-databases/work_with_database/phones/views.py
@@ -1,17 +1,23 @@
 from django.shortcuts import render, redirect
-
+from django.conf import settings
+from phones.models import Phone
 
 def index(request):
     return redirect('catalog')
 
-
 def show_catalog(request):
     template = 'catalog.html'
-    context = {}
+    all_phones = Phone.objects.all()
+    if request.GET.get('sort') == 'name':
+        phones = all_phones.order_by('name')
+    elif request.GET.get('sort') == 'min_price':
+        phones = all_phones.order_by('price')
+    elif request.GET.get('sort') == 'max_price':
+        phones = all_phones.order_by('-price')
+    context = {'phones': phones}
     return render(request, template, context)
-
 
 def show_product(request, slug):
     template = 'product.html'
-    context = {}
+    context = {'phone': Phone.objects.filter(slug=slug).first()}
     return render(request, template, context)
