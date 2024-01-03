082201ddf4cc2373a11a9979f16ca10d6f571ee5
Evgeny Bor <evbor@mail.ru>
2024-01-03 14:11:03 +0500

    hw3.1

--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,23 @@
 # TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
 # TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+
+from rest_framework.generics import RetrieveUpdateAPIView, ListCreateAPIView, RetrieveAPIView
+
+from measurement.models import Sensor, Measurement
+from measurement.serializers import MeasurementSerializer, SensorDetailSerializer
+
+class CreateViewSensor(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+class UpdateSensor(RetrieveUpdateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+class UpdateMeasurment(ListCreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+class OneSensor(RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
